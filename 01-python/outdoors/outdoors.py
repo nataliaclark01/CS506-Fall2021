9db0f2ee8796030422e7d707244cd5d6b9ce12b1
nataliaclark01 <79187383+nataliaclark01@users.noreply.github.com>
2021-09-23 13:05:35 -0400

    adding trees function

--- 01-python/outdoors/outdoors.py
+++ 01-python/outdoors/outdoors.py
@@ -1,7 +1,10 @@
 from .lake import draw_lake
 from .park import draw_park
+from .tree import add_trees
 
 def draw_outdoors():
+    add_trees()
     draw_lake()
     draw_park()
+    add_trees()
     return
