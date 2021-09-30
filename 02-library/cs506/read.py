8e0e2b5612c09e39edf4446aa3b471533228680d
nataliaclark01 <79187383+nataliaclark01@users.noreply.github.com>
2021-09-30 13:09:11 -0400

    adding read_csv function

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,9 @@
+import pandas as pd
+
 def read_csv(csv_file_path):
-    """
-        Given a path to a csv file, return a matrix (list of lists)
-        in row major.
-    """
-    raise NotImplementedError()
+    data = pd.read(csv_file_path)
+    matrix = []
+    for x in range(len(data)):
+        for y in range(len(data[0])):
+            matrix[x][y] = data[x][y]
+    return matrix
