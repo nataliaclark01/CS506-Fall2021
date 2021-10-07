98824e6597b8f41ffbbb1d166c55c05568ab4126
nataliaclark01 <79187383+nataliaclark01@users.noreply.github.com>
2021-10-07 12:58:05 -0400

    committing rest to repo

--- 01-python/outdoors/outdoors.py
+++ 01-python/outdoors/outdoors.py
@@ -1,7 +1,17 @@
 from .lake import draw_lake
 from .park import draw_park
+<<<<<<< Updated upstream
 
 def draw_outdoors():
     draw_lake()
     draw_park()
+=======
+from .tree import draw_tree
+
+def draw_outdoors():
+    draw_tree()
+    draw_lake()
+    draw_park()
+    draw_tree()
+>>>>>>> Stashed changes
     return
